5c7f3a0c46531fe7cd83ebfef650d14fa011b6ad
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-09 17:11:49 +0300

    unpdated crowdsale contract

--- contracts/MRC_Crowdsale.sol
+++ contracts/MRC_Crowdsale.sol
@@ -1,7 +1,46 @@
 pragma solidity ^0.4.24;
 
+import "../node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 
-contract MRC_Crowdsale {
-  constructor() public {
+import "./MRC_WhitelistedSourceDestination.sol";
+import "./MRC_Token.sol";
+
+
+contract MRC_Crowdsale is Crowdsale, Pausable, MRC_WhitelistedSourceDestination {
+  
+  constructor(uint256 _rate, address _wallet, ERC20 _token) Crowdsale(_rate, _wallet, _token) public {
+      
   }
+
+  /**
+   * PUBLIC
+   */
+
+
+
+  /**
+   * OVERRIDEN
+   */
+
+   /**
+   * @dev Extend parent behavior requiring beneficiary to be in whitelist.
+   * @param _beneficiary Token beneficiary
+   * @param _weiAmount Amount of wei contributed
+   */
+  function _preValidatePurchase(
+    address _beneficiary,
+    uint256 _weiAmount
+  )
+    internal
+    isWhitelisted(_beneficiary)
+  {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+  }
+
+
+
+  /**
+   * PRIVATE
+   */
 }
