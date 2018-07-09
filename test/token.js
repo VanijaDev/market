796ada4b26ff84472edcb773d13d83954a4e0dbe
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-09 18:30:23 +0300

    Added tests for token

--- test/token.js
+++ test/token.js
@@ -1,24 +1,35 @@
-let MRC_Token = artifacts.require("./MRC_Token");
+const MRC_Token = artifacts.require("MRC_Token");
+
+const BigNumber = require('bignumber.js');
 
 import {
   advanceBlock
 } from './helpers/advanceToBlock.js';
 
-var BigNumber = require('bignumber.js');
+import expectThrow from './helpers/expectThrow';
 
 
-contract("MRC_Token", () => {
+contract("MRC_Token", (accounts) => {
   let token;
+  const ACC_1 = accounts[1];
 
   before("setup", async () => {
-    advanceBlock();
+    await advanceBlock();
+  });
+
+  beforeEach("new token inst", async () => {
+    token = await MRC_Token.new();
   });
 
   it("should validate total supply", async () => {
-    // assert.equal(new BigNumber(await token.totalSupplyMax.call()).toNumber(), new BigNumber(100000000000000000).toNumber(), "wrong totalSupplyMax");
+    assert.equal(new BigNumber(await token.totalSupplyMax.call()).toNumber(), new BigNumber(100000000000000000).toNumber(), "wrong totalSupplyMax");
   });
 
   it("should not mint more than max limit", async () => {
+    let limit = new BigNumber(await token.totalSupplyMax.call());
+    let limit_2_of_3 = limit.multipliedBy(2).dividedBy(3);
 
+    await token.mint(ACC_1, limit_2_of_3.toNumber());
+    await expectThrow(token.mint(ACC_1, limit_2_of_3.toNumber()), "should not allow to mint more than limit");
   });
 });
