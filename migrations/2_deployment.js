fb43590dc77cc7a1018b123e9b47494df4b41e3c
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-10 17:59:31 +0300

    added crowdsale to deployment

--- migrations/2_deployment.js
+++ migrations/2_deployment.js
@@ -1,7 +1,12 @@
 let MRC_Token = artifacts.require("./MRC_Token.sol");
 let MRC_Crowdsale = artifacts.require("./MRC_Crowdsale.sol");
 
-//  tODO: () =>
-module.exports = function (deployer) {
-    deployer.deploy(MRC_Token);
+
+module.exports = async function (deployer, network, accounts) {
+    const WALLET = accounts[9];
+
+    await deployer.deploy(MRC_Token);
+    let token = await IMP_Token.deployed();
+
+    await deployer.deploy(MRC_Crowdsale, 1500, WALLET, token.address, [14, 5, 15, 1]);
 }
