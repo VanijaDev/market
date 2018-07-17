4ee3fcbe78b9189ca997993bab68b1c87ac1d8f3
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-17 15:27:53 +0300

    contants created

--- migrations/2_deployment.js
+++ migrations/2_deployment.js
@@ -4,9 +4,15 @@ let MRC_Crowdsale = artifacts.require("./MRC_Crowdsale.sol");
 
 module.exports = async function (deployer, network, accounts) {
     const WALLET = accounts[9];
+    const RATE = 1500; //  tokens per ETH
 
     await deployer.deploy(MRC_Token);
-    let token = await IMP_Token.deployed();
+    let token = await MRC_Token.deployed();
 
-    await deployer.deploy(MRC_Crowdsale, 1500, WALLET, token.address, [14, 5, 15, 1]);
+    const RESERVE_TEAM = 14;
+    const RESERVE_BOUNTY = 5;
+    const RESERVE_DEVELOPMENT = 15;
+    const RESERVE_SALE_COST = 1;
+
+    await deployer.deploy(MRC_Crowdsale, RATE, WALLET, token.address, [RESERVE_TEAM, RESERVE_BOUNTY, RESERVE_DEVELOPMENT, RESERVE_SALE_COST]);
 }
