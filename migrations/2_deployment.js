a7e41ed64d2e247137c35951b1e248cfbd68b98c
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-18 12:29:14 +0300

    prepared migration for timimgs

--- migrations/2_deployment.js
+++ migrations/2_deployment.js
@@ -1,19 +1,27 @@
 let MRC_Token = artifacts.require("./MRC_Token.sol");
 let MRC_Crowdsale = artifacts.require("./MRC_Crowdsale.sol");
 
+let IncreaseTime = require("../test/helpers/increaseTime.js");
 
 module.exports = async function (deployer, network, accounts) {
     const WALLET = accounts[9];
     const RATE = 1500; //  tokens per ETH
 
-    await deployer.deploy(MRC_Token);
-    let token = await MRC_Token.deployed();
+    await deployer.deploy(MRC_Token).then(async () => {
+        let token = await MRC_Token.deployed();
 
-    const RESERVE_TEAM = 14;
-    const RESERVE_BOUNTY = 5;
-    const RESERVE_DEVELOPMENT = 15;
-    const RESERVE_SALE_COST = 1;
+        const OPENING = web3.eth.getBlock("latest").timestamp;
+        const ICO_START = OPENING + IncreaseTime.duration.hours(1);
+        const CLOSING = ICO_START + IncreaseTime.duration.hours(1);
+        const TIMINGS = [OPENING, ICO_START, CLOSING];
 
-    let crowdsale = await deployer.deploy(MRC_Crowdsale, RATE, WALLET, token.address, [RESERVE_TEAM, RESERVE_BOUNTY, RESERVE_DEVELOPMENT, RESERVE_SALE_COST]);
-    await token.transferOwnership(crowdsale.address);
+        const RESERVE_TEAM = 14;
+        const RESERVE_BOUNTY = 5;
+        const RESERVE_DEVELOPMENT = 15;
+        const RESERVE_SALE_COST = 1;
+        const RESERVATIONS = [RESERVE_TEAM, RESERVE_BOUNTY, RESERVE_DEVELOPMENT, RESERVE_SALE_COST];
+
+        let crowdsale = await deployer.deploy(MRC_Crowdsale, RATE, WALLET, token.address, RESERVATIONS);
+        await token.transferOwnership(crowdsale.address);
+    });
 }
