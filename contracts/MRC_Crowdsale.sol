16df127c21487b22486a8cfdb08b035cdf0e6a80
Ivan Solomichev <vanijatko.social@gmail.com>
2018-07-18 12:55:50 +0300

    Added smart comments

--- contracts/MRC_Crowdsale.sol
+++ contracts/MRC_Crowdsale.sol
@@ -9,9 +9,17 @@ import "./MRC_Token.sol";
 
 
 contract MRC_Crowdsale is MRC_StagesCrowdsale, Pausable, MRC_WhitelistedSourceDestination, MRC_CrowdsaleReservations {
-  MRC_Token token;  //  TODO: remove
+  MRC_Token token;
 
   
+  /**
+   * @dev Constructor, takes crowdsale opening and closing times.
+   * @param _rate                 Crowdsale token per ETH rate
+   * @param _wallet               Crowdsale wallet
+   * @param _token                Crowdsale token
+   * @param _reservationPercents  Crowdsale res: [RESERVE_TEAM, RESERVE_BOUNTY, RESERVE_DEVELOPMENT, RESERVE_SALE_COST]
+   * @param _timings              Crowdsale timings: [OPENING, ICO_START, CLOSING]
+   */
   constructor(uint256 _rate, address _wallet, ERC20 _token, uint8[] _reservationPercents, uint256[] _timings) MRC_StagesCrowdsale(_rate, _wallet, _token, _timings) public {
     token = MRC_Token(_token);
     calculateTokenReservations(_reservationPercents, token.totalSupplyMax());
--- contracts/MRC_StagesCrowdsale.sol
+++ contracts/MRC_StagesCrowdsale.sol
@@ -7,8 +7,18 @@ import "./MRC_Token.sol";
 
 
 contract MRC_StagesCrowdsale is TimedCrowdsale {
+
+  /**
+   * @dev Constructor, takes crowdsale opening and closing times.
+   * @param _rate     Crowdsale token per ETH rate
+   * @param _wallet   Crowdsale wallet
+   * @param _token    Crowdsale token
+   * @param _timings  Crowdsale timings: [OPENING, ICO_START, CLOSING]
+   */
   constructor(uint256 _rate, address _wallet, ERC20 _token, uint256[] _timings) 
     Crowdsale(_rate, _wallet, _token)
     TimedCrowdsale(_timings[0], _timings[2]) public {
+
+
   }
 }
