92a177ecbb8d05399e88ecac78ecd9bea2854212
Ivan Solomichev <vanijatko.social@gmail.com>
2018-09-25 17:24:21 +0300

    updated tests

--- test/3_crowdsale_reservations.js
+++ test/3_crowdsale_reservations.js
@@ -33,7 +33,7 @@ contract("Reservations", (accounts) => {
     const CLOSING = ICO_START + duration.hours(1);
     const TIMINGS = [OPENING, ICO_START, CLOSING];
 
-    crowdsale = await MRC_Crowdsale.new(mock.rate, token.address, wallet, mock.reservations, TIMINGS);
+    crowdsale = await MRC_Crowdsale.new(mock.rate, token.address, wallet, TIMINGS);
     await token.transferOwnership(crowdsale.address);
   });
 
@@ -59,27 +59,77 @@ contract("Reservations", (accounts) => {
     });
   });
 
+  describe("not owner is able to transfer reservations", () => {
+    it("should throw if not owner tries to transfer reservations", async () => {
+      await expectThrow(crowdsale.transferReservationTeam(ACC_1, {
+        from: ACC_1
+      }), "not owner is not allowed to transfer reservations");
+    });
+  });
+
+  describe("validate reservations transfer success", () => {
+    it("should validate reservation Team delivered succesfully", async () => {
+      let tokenReserve = new BigNumber(await crowdsale.tokensReservedFor(0));
+      let tokensBefore = new BigNumber(await token.balanceOf(ACC_1));
+
+      await crowdsale.transferReservationTeam(ACC_1);
+
+      let tokensAfter = new BigNumber(await token.balanceOf(ACC_1));
+      assert.equal(tokensAfter.minus(tokensBefore).toNumber(), tokenReserve.toNumber(), "wrong tokens transferred during reservation Team");
+    });
+
+    it("should validate reservation Bounty delivered succesfully", async () => {
+      let tokenReserve = new BigNumber(await crowdsale.tokensReservedFor(1));
+      let tokensBefore = new BigNumber(await token.balanceOf(ACC_1));
+
+      await crowdsale.transferReservationBounty(ACC_1);
+
+      let tokensAfter = new BigNumber(await token.balanceOf(ACC_1));
+      assert.equal(tokensAfter.minus(tokensBefore).toNumber(), tokenReserve.toNumber(), "wrong tokens transferred during reservation Bounty");
+    });
+
+    it("should validate reservation Development delivered succesfully", async () => {
+      let tokenReserve = new BigNumber(await crowdsale.tokensReservedFor(2));
+      let tokensBefore = new BigNumber(await token.balanceOf(ACC_1));
+
+      await crowdsale.transferReservationDevelopment(ACC_1);
+
+      let tokensAfter = new BigNumber(await token.balanceOf(ACC_1));
+      assert.equal(tokensAfter.minus(tokensBefore).toNumber(), tokenReserve.toNumber(), "wrong tokens transferred during reservation Development");
+    });
+
+    it("should validate reservation Sale delivered succesfully", async () => {
+      let tokenReserve = new BigNumber(await crowdsale.tokensReservedFor(3));
+      let tokensBefore = new BigNumber(await token.balanceOf(ACC_1));
+
+      await crowdsale.transferReservationSale(ACC_1);
+
+      let tokensAfter = new BigNumber(await token.balanceOf(ACC_1));
+      assert.equal(tokensAfter.minus(tokensBefore).toNumber(), tokenReserve.toNumber(), "wrong tokens transferred during reservation Sale");
+    });
+  });
+
   describe("reservations got cleared after transfer", () => {
     it("should validate no team reservation team after transfer", async () => {
-      await crowdsale.transferTeamReservation(ACC_1);
+      await crowdsale.transferReservationTeam(ACC_1);
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(0));
       assert.equal(tokens.toNumber(), 0, "team reservation should be cleared after transfer");
     });
 
     it("should validate no bounty reservation team after transfer", async () => {
-      await crowdsale.transferBountyReservation(ACC_1);
+      await crowdsale.transferReservationBounty(ACC_1);
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(1));
       assert.equal(tokens.toNumber(), 0, "bounty reservation should be cleared after transfer");
     });
 
     it("should validate no development reservation team after transfer", async () => {
-      await crowdsale.transferDevelopmentReservation(ACC_1);
+      await crowdsale.transferReservationDevelopment(ACC_1);
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(2));
       assert.equal(tokens.toNumber(), 0, "development reservation should be cleared after transfer");
     });
 
     it("should validate no token sale reservation team after transfer", async () => {
-      await crowdsale.transferSaleReservation(ACC_1);
+      await crowdsale.transferReservationSale(ACC_1);
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(3));
       assert.equal(tokens.toNumber(), 0, "token sale reservation should be cleared after transfer");
     });
@@ -89,7 +139,7 @@ contract("Reservations", (accounts) => {
     it("should validate team reservation transfer event on transfer", async () => {
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(0));
 
-      let tx = await crowdsale.transferTeamReservation(ACC_1);
+      let tx = await crowdsale.transferReservationTeam(ACC_1);
       let logs = tx.logs;
 
       assert.equal(logs.length, 1, "should be 1 event");
@@ -102,7 +152,7 @@ contract("Reservations", (accounts) => {
     it("should validate bounty reservation transfer event on transfer", async () => {
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(1));
 
-      let tx = await crowdsale.transferBountyReservation(ACC_1);
+      let tx = await crowdsale.transferReservationBounty(ACC_1);
       let logs = tx.logs;
 
       assert.equal(logs.length, 1, "should be 1 event");
@@ -115,7 +165,7 @@ contract("Reservations", (accounts) => {
     it("should validate development reservation transfer event on transfer", async () => {
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(2));
 
-      let tx = await crowdsale.transferDevelopmentReservation(ACC_1);
+      let tx = await crowdsale.transferReservationDevelopment(ACC_1);
       let logs = tx.logs;
 
       assert.equal(logs.length, 1, "should be 1 event");
@@ -128,7 +178,7 @@ contract("Reservations", (accounts) => {
     it("should validate sale cost reservation transfer event on transfer", async () => {
       let tokens = new BigNumber(await crowdsale.tokensReservedFor(3));
 
-      let tx = await crowdsale.transferSaleReservation(ACC_1);
+      let tx = await crowdsale.transferReservationSale(ACC_1);
       let logs = tx.logs;
 
       assert.equal(logs.length, 1, "should be 1 event");
@@ -136,6 +186,12 @@ contract("Reservations", (accounts) => {
       assert.equal(log.event, "SaleReserveTransferred", "wrong event on sale cost reservation transfer");
       assert.equal(log.args._address, ACC_1, "wrong address on event on sale cost reservation transfer");
       assert.equal(new BigNumber(log.args._amount).toNumber(), tokens.toNumber(), "wrong tokens on event on sale cost reservation transfer");
+    });
+  });
+
+  describe("modifier validation", () => {
+    it("should validate nonZeroAddressOnly modifier", async () => {
+      await expectThrow(crowdsale.transferReservationSale(0), "should throw if beneficiary address == 0");
     });
   });
 });
