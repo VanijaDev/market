f86a807c23d4c401ccea6d69441d27f0e92619a6
Ivan Solomichev <vanijatko.social@gmail.com>
2018-09-27 17:12:30 +0300

    to prev commit

--- migrations/2_deployment.js
+++ migrations/2_deployment.js
@@ -5,7 +5,7 @@ let IncreaseTime = require("../test/helpers/increaseTime.js");
 
 module.exports = async function (deployer, network, accounts) {
     const WALLET = accounts[9];
-    const RATE = 1500; //  tokens per ETH
+    const RATE_ETH = [1500, 500]; //  tokens per ETH
 
     await deployer.deploy(MRC_Token).then(async () => {
         let token = await MRC_Token.deployed();
@@ -15,7 +15,7 @@ module.exports = async function (deployer, network, accounts) {
         const CLOSING = ICO_START + IncreaseTime.duration.hours(1); //  TODO: set before deploy
         const TIMINGS = [OPENING, ICO_START, CLOSING];
 
-        let crowdsale = await deployer.deploy(MRC_Crowdsale, RATE, token.address, WALLET, TIMINGS);
+        let crowdsale = await deployer.deploy(MRC_Crowdsale, RATE_ETH, token.address, WALLET, TIMINGS);
         await token.transferOwnership(crowdsale.address);
     });
 }
